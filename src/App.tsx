49c8866641de612eb23dd2cf97b433a405b484e6
Patricia Magalhães <paath.magalhaes2@gmail.com>
2023-07-19 17:27:55 -0300

    test: app component rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState(
+    "Let's learn more about testing in React"
+  );
+
   return (
     <div>
       <h1>Hello world!</h1>
-      <p>Let's learn more about testing in React</p>
+      <p>{message}</p>
+      <button
+        style={{ backgroundColor: "blue", color: "white", padding: 10 }}
+        onClick={() => setMessage("New message!")}
+      >
+        Change message
+      </button>
     </div>
   );
 }
